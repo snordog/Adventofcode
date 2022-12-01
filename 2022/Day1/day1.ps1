986849842dbc7c589395b758e47b77d472a00afd
snordog <snordog101@hotmail.com>
2022-12-01 07:57:34 -0800

    Add completed scripts

--- 2022/Day1/day1.ps1
+++ 2022/Day1/day1.ps1
@@ -1,6 +1,6 @@
 #Day1
 
-$CalorieTotalArray = (((Get-Content ./day1_part1_input.txt -Raw) -split "\n\n") | ForEach-Object {($_ -split "\n" | Measure-Object -Sum).Sum} | Sort-Object -Descending)
+$CalorieTotalArray = (((Get-Content "/home/corox/Scripts/Adventofcode/2022/Day1/day1_part1_input.txt" -Raw) -split "\n\n") | ForEach-Object {($_ -split "\n" | Measure-Object -Sum).Sum} | Sort-Object -Descending)
 
 Write-Host "Day 1 p1: $($CalorieTotalArray[0])"
 Write-Host "Day 2 p2: $($CalorieTotalArray[0] + $CalorieTotalArray[1] + $CalorieTotalArray[2])"
